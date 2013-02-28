8abb4d28161a721c61f2aca77b6dc7f12aa805a7
anshaoyuan <anshaoyuan@gmail.com>
2013-02-28 19:47:29 +0800

    添加博文表

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130228112534) do
+ActiveRecord::Schema.define(:version => 20130228114359) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.string   "user_id"
+    t.string   "integer"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
